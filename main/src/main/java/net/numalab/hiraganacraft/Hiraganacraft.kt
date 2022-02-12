c56def11c7709f1f074e7a7a859122236450795a
bun133 <bun133.work@gmail.com>
2022-02-12 18:34:08 +0900

    チケット2:爆発でアイテムがひらがなカードになるように

--- main/src/main/java/net/numalab/hiraganacraft/Hiraganacraft.kt
+++ main/src/main/java/net/numalab/hiraganacraft/Hiraganacraft.kt
@@ -5,8 +5,15 @@ import dev.kotx.flylib.flyLib
 import net.kyori.adventure.text.Component
 import net.numalab.hiraganacraft.command.MainCommand
 import org.bukkit.Material
+import org.bukkit.NamespacedKey
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.Item
 import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.entity.EntityDeathEvent
+import org.bukkit.event.entity.EntityEvent
+import org.bukkit.event.entity.ExplosionPrimeEvent
 import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataType
 import org.bukkit.plugin.java.JavaPlugin
 import java.awt.TextComponent
 import kotlin.streams.toList
@@ -37,6 +44,25 @@ class Hiraganacraft : JavaPlugin() {
                 }
             }
         })
+
+        flyLibBuilder.listen(ExplosionPrimeEvent::class.java, action = {
+            val items = it.entity.location.getNearbyEntitiesByType(Item::class.java, it.radius.toDouble())
+            val itemStacks = items.map { i -> i.itemStack }.filter { s ->
+                !isGeneratedItemStack(s)
+            }
+            val toDrop = itemStacks.map { s -> intoHiraganaCards(s) }.flatten()
+            val location = it.entity.location.clone()
+            server.scheduler.runTaskLater(this@Hiraganacraft, Runnable {
+                toDrop.forEach { stack ->
+                    location.world.dropItem(location, stack)
+                }
+            }, 1)
+        })
+    }
+
+    private fun intoHiraganaCards(itemStack: ItemStack): List<ItemStack> {
+        val converted = converter[itemStack.type] ?: return listOf()
+        return generateItemStacks(converted).onEach { it.amount = itemStack.amount }
     }
 
     private fun generateItemStacks(str: String): List<ItemStack> =
@@ -49,7 +75,27 @@ class Hiraganacraft : JavaPlugin() {
             it.displayName(Component.text(char))
             it.setCustomModelData(customDataInt)
         }
+        markItemStack(item)
         return item
+    }
+
+    private fun isGeneratedItemStack(itemStack: ItemStack): Boolean {
+        return itemStack.type == Material.PAPER && isMarkedItemStack(itemStack)
+    }
+
+    private val nameKey = NamespacedKey(this, "itemmarker")
+    private fun markItemStack(itemStack: ItemStack) {
+        itemStack.editMeta {
+            it.persistentDataContainer.set(nameKey, PersistentDataType.INTEGER, 1)
+        }
+    }
+
+    private fun isMarkedItemStack(itemStack: ItemStack): Boolean {
+        val meta = itemStack.itemMeta
+        return meta.persistentDataContainer.has(
+            nameKey,
+            PersistentDataType.INTEGER
+        ) && meta.persistentDataContainer.get(nameKey, PersistentDataType.INTEGER) == 1
     }
 
     override fun onEnable() {
