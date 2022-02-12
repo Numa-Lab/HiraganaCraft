ef830b29adda1048d522eedc98f0969a0f39fe8f
bun133 <bun133.work@gmail.com>
2022-02-12 18:51:18 +0900

    チケット3:ブロック破壊時のドロップに確率をつける

--- main/src/main/java/net/numalab/hiraganacraft/HiraganaConfig.kt
+++ main/src/main/java/net/numalab/hiraganacraft/HiraganaConfig.kt
@@ -2,8 +2,11 @@ package net.numalab.hiraganacraft
 
 import net.kunmc.lab.configlib.BaseConfig
 import net.kunmc.lab.configlib.value.BooleanValue
+import net.kunmc.lab.configlib.value.DoubleValue
 import org.bukkit.plugin.Plugin
 
 class HiraganaConfig(plugin: Plugin) : BaseConfig(plugin) {
     val isEnabled = BooleanValue(false)
+    // ドロップする確率(%)
+    val dropRate = DoubleValue(50.0)
 }
