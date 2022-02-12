1ea5c61538b89133dec0c5a553d1d2515da8654d
bun133 <bun133.work@gmail.com>
2022-02-12 17:26:12 +0900

    チケット1:ブロック破壊時にひらがなカードが落ちるように

---
+++ main/src/main/java/net/numalab/hiraganacraft/HiraganaConverter.kt
@@ -0,0 +1,167 @@
+package net.numalab.hiraganacraft
+
+import com.google.gson.Gson
+import com.google.gson.JsonElement
+import com.google.gson.JsonParser
+import org.bukkit.Material
+import org.bukkit.NamespacedKey
+import org.bukkit.inventory.ItemStack
+import java.io.Reader
+import java.lang.Exception
+
+class HiraganaConverter(private val jaFileReader: Reader) {
+    private val gson = Gson()
+    private var json = loadFile()
+
+    /**
+     * Load Ja_Jp from File
+     */
+    private fun loadFile(): Map<String, String>? {
+        return try {
+            val rootElement = gson.fromJson(jaFileReader, JsonElement::class.java)
+            val jsonObject = rootElement.asJsonObject
+            jsonObject.entrySet().toMutableList().associate { it.key to it.value.asString }
+        } catch (e: Exception) {
+            // Failed
+            null
+        }
+    }
+
+    // ItemStack -> Material -> String
+    operator fun get(str: String): String? {
+        val value = json?.get(str)
+        if (value != null) {
+            return mapString(value)
+        } else {
+            return null
+        }
+    }
+
+    operator fun get(material: Material) = get(material.translationKey)
+    operator fun get(itemStack: ItemStack) = get(itemStack.type)
+
+    private fun validateChar(char: Char): Boolean {
+        return map.keys.contains(char)
+    }
+
+    fun toCustomModelDataInt(char: Char): Int? {
+        val mappedChar = mapChar(char)
+        return map[mappedChar]
+    }
+
+    /**
+     * Convert String to Valid Chars
+     */
+    fun toChars(str: String): List<Char> {
+        return str.toCharArray().toMutableList().map { mapChar(it) }.filter { validateChar(it) }
+    }
+
+    fun mapString(str: String): String {
+        return toChars(str).joinToString("")
+    }
+
+    /**
+     * 文字を変換
+     * 小文字　→　大文字
+     * 濁点、半濁点　→　取る
+     */
+    fun mapChar(char: Char): Char {
+        when (char) {
+            'ぁ' -> return 'あ'
+            'ぇ' -> return 'え'
+            'ぃ' -> return 'い'
+            'ぉ' -> return 'お'
+            'っ' -> return 'つ'
+            'ぅ' -> return 'う'
+            'ゃ' -> return 'や'
+            'ょ' -> return 'よ'
+            'ゅ' -> return 'ゆ'
+            'ゎ' -> return 'わ'
+            'が' -> return 'か'
+            'ぎ' -> return 'き'
+            'ぐ' -> return 'ぐ'
+            'げ' -> return 'け'
+            'ご' -> return 'こ'
+            'ざ' -> return 'さ'
+            'じ' -> return 'し'
+            'ず' -> return 'す'
+            'ぜ' -> return 'せ'
+            'ぞ' -> return 'そ'
+            'だ' -> return 'た'
+            'ぢ' -> return 'ち'
+            'づ' -> return 'つ'
+            'で' -> return 'て'
+            'ど' -> return 'と'
+            'ば' -> return 'は'
+            'び' -> return 'ひ'
+            'ぶ' -> return 'ふ'
+            'べ' -> return 'へ'
+            'ぼ' -> return 'ほ'
+            'ぱ' -> return 'は'
+            'ぴ' -> return 'ひ'
+            'ぷ' -> return 'ふ'
+            'ぺ' -> return 'へ'
+            'ぽ' -> return 'ほ'
+            else -> return char
+        }
+    }
+
+    private val map = listOf(
+        'あ',
+        'ち',
+        'え',
+        'ふ',
+        'は',
+        'へ',
+        'ひ',
+        'ほ',
+        'い',
+        'か',
+        'け',
+        'き',
+        'こ',
+        'く',
+        'ま',
+        'め',
+        'み',
+        'も',
+        'む',
+        'ん',
+        'な',
+        'ね',
+        'に',
+        'の',
+        'ぬ',
+        'お',
+        'ら',
+        'れ',
+        'り',
+        'ろ',
+        'る',
+        'さ',
+        'せ',
+        'し',
+        'そ',
+        'す',
+        'た',
+        'て',
+        'と',
+        'つ',
+        'う',
+        'わ',
+        'を',
+        'や',
+        'よ',
+        'ゆ',
+        'ー',
+        'ぁ',
+        'ぇ',
+        'ぃ',
+        'ぉ',
+        'っ',
+        'ぅ',
+        'ゃ',
+        'ょ',
+        'ゅ'
+    ).mapIndexed { index, c -> c to (index + 1) }.toMap()
+}
--- main/src/main/java/net/numalab/hiraganacraft/Hiraganacraft.kt
+++ main/src/main/java/net/numalab/hiraganacraft/Hiraganacraft.kt
@@ -1,18 +1,55 @@
 package net.numalab.hiraganacraft
 
+import dev.kotx.flylib.FlyLibBuilder
 import dev.kotx.flylib.flyLib
+import net.kyori.adventure.text.Component
 import net.numalab.hiraganacraft.command.MainCommand
+import org.bukkit.Material
+import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
+import java.awt.TextComponent
+import kotlin.streams.toList
 
 class Hiraganacraft : JavaPlugin() {
-    private val config = HiraganaConfig(this)
+    private val config = HiraganaConfig(this).also {
+        it.saveConfigIfAbsent()
+        it.loadConfig()
+    }
+
+    private val converter = HiraganaConverter(this.getTextResource("ja_jp.json")!!)
 
     init {
-        config.saveConfigIfAbsent()
-        config.loadConfig()
         flyLib {
             command(MainCommand(config))
+            listens(this)
         }
+    }
+
+    private fun listens(flyLibBuilder: FlyLibBuilder) {
+        // ブロック破壊時にひらがなカードが落ちるように
+        flyLibBuilder.listen(BlockBreakEvent::class.java, action = {
+            if (config.isEnabled.value() && it.isDropItems) {
+                it.isDropItems = false
+                val toDrop = converter[it.block.type]?.let { str -> generateItemStacks(str) }
+                toDrop?.forEach { stack ->
+                    it.block.location.world.dropItem(it.block.location, stack)
+                }
+            }
+        })
+    }
+
+    private fun generateItemStacks(str: String): List<ItemStack> =
+        converter.toChars(str).mapNotNull { generateItemStack(it) }
+
+    private fun generateItemStack(char: Char): ItemStack? {
+        val customDataInt = converter.toCustomModelDataInt(char) ?: return null
+        val item = ItemStack(Material.PAPER)
+        item.editMeta {
+            it.displayName(Component.text(char))
+            it.setCustomModelData(customDataInt)
+        }
+        return item
     }
 
     override fun onEnable() {
